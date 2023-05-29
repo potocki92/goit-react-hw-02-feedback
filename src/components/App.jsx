2f35cf9a98755a7eab6bc3fe8a250b92ad6aa9c9
potocki92 <mateusz.potocki92@gmail.com>
2023-05-29 21:55:33 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,18 @@
-import React from 'react';
-import Section from './Section/Section';
-import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
-import Notification from './Notification/Notification';
-import Statistics from './Statisticts/Statistics';
+import Feedback from "./Feedback/Feedback";
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  handleFeedback = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return total ? Math.round((good / total) * 100) : 0;
-  };
-
-  render() {
-    return (
-      <div>
-        <Section title="Expresso Cafe Feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleFeedback}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          {this.countTotalFeedback() ? (
-            <Statistics
-              {...this.state}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
-
-export default App;
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <Feedback />
+    </div>
+  );
+};
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  return <div>
-    <h2>Give feedback</h2>
-    {options.map(option => (
-        <button key={option} type="button" onClick={() => onLeaveFeedback(option)}>
+  return (
+    <div className={styles.feedbackOptions}>
+        <h3 className={styles.heading}>Please leave feedback</h3>
+      <div className={styles.buttonContainer}>
+        {options.map(option => (
+          <button
+            className={styles.button}
+            key={option}
+            type="button"
+            onClick={() => onLeaveFeedback(option)}
+          >
             {option}
-        </button>
-    ))}
-  </div>;
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackOptions;
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
